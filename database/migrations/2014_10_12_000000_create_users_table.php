e63e7515a69b0e922d9001f19a0d1844f3e5b0e3
danieletulone <danieletulone.work@gmail.com>
2020-04-27 16:07:38 +0200

    Installed passport and removed old UserController

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,19 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-						$table->string('nickname');
-            $table->string('name');
-						$table->string('surname');
-						$table->string('password');
-						$table->integer('points');
-
+            $table->string('firstname');
+						$table->string('lastname');
+            $table->string('password');
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
 
-						$table->bigInteger('role_id')->unsigned();
+						$table->bigInteger('role_id')->unsigned()->nullable();
 						$table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
 
-						$table->bigInteger('image_id')->unsigned();
+						$table->bigInteger('image_id')->unsigned()->nullable();
 						$table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
 
 						$table->rememberToken();
