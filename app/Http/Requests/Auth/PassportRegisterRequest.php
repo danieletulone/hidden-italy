6ff3b7a01d272e1e00ee3497fc62e05d376b6acf
danieletulone <danieletulone.work@gmail.com>
2020-05-08 03:11:32 +0200

    Add more email validation. #2

--- app/Http/Requests/Auth/PassportRegisterRequest.php
+++ app/Http/Requests/Auth/PassportRegisterRequest.php
@@ -26,7 +26,7 @@ class PassportRegisterRequest extends FormRequest
         return [
             'firstname' => ['required', 'string', 'max:255'],
             'lastname'  => ['required', 'string', 'max:255'],
-            'email'     => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email'     => ['required', 'string', 'email:rfc,filter', 'max:255', 'unique:users'],
             'password'  => ['required', 'string', 'min:8', 'confirmed'],
         ];
     }
