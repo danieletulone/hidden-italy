b8426faa5048a5030751449abbb34afcdb3e20d3
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-05-17 21:21:51 +0200

    Add image into seeds monuments. #3

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -14,12 +14,136 @@ class ImagesTableSeeder extends Seeder
     {
         // factory(Image::class, 10)->create();
 
-        Image::insert([
-        'title' => '',
-        'description' => '',
-        'url' => '',
-        'monument_id' => '',
-        'user_id' => ''
-        ]);
-        }
+				/*
+				MONUMENT
+				1 : Arco della pace
+				2 : Triennale di Milano
+				3 : Bosco Verticale
+				4 : Duomo di Milano
+				5 : Colonne di San Lorenzo
+				6 : Piazza Affari
+				7 : Chiesa di Santa Maria presso San Satiro
+				*/
+
+				Image::insert([
+				'title' => 'Arco della pace',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/arco1.jpg',
+				'monument_id' => '1',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Arco della pace',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/arco2.jpg',
+				'monument_id' => '1',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Triennale di Milano',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/triennale1.jpg',
+				'monument_id' => '2',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Triennale di Milano',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/triennale2.jpg',
+				'monument_id' => '2',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Triennale di Milano',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/triennale3.jpg',
+				'monument_id' => '2',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Bosco Verticale',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/bosco1.jpg',
+				'monument_id' => '3',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Bosco Verticale',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/bosco2.jpg',
+				'monument_id' => '3',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Duomo di Milano',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/duomo1.jpg',
+				'monument_id' => '4',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Duomo di Milano',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/duomo2.jpg',
+				'monument_id' => '4',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Colonne di San Lorenzo',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/colonne1.jpg',
+				'monument_id' => '5',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Colonne di San Lorenzo',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/colonne2.jpg',
+				'monument_id' => '5',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Piazza Affari',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/affari1.jpg',
+				'monument_id' => '6',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Piazza Affari',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/affari2.jpg',
+				'monument_id' => '6',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Chiesa di Santa Maria presso San Satiro',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/chiesa1.jpg',
+				'monument_id' => '7',
+				'user_id' => '1',
+				]);
+
+				Image::insert([
+				'title' => 'Chiesa di Santa Maria presso San Satiro',
+				'description' => 'Questa è l\'immagine del monumento',
+				'url' => 'seedImage/chiesa2.jpg',
+				'monument_id' => '7',
+				'user_id' => '1',
+				]);
+		}
+
 }
