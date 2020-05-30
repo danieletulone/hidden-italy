32b31e0a48c4c9e18248b372908dcae36e6c9790
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-05-30 20:51:35 +0200

    Fix create input (description placeholder).

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,7 +3,7 @@
 <x-container>
 	<x-back-button />
 	<x-form :action="route('categories.store')" btnText='add.categories'>
-		<x-input name="description" type="text" placeholder="Description" />
+		<x-input name="description" type="text" placeholder="description" />
 	</x-form>
 </x-container>
 @endsection
