ba22ef604009abc988268ad87cf5a49824a5ea33
Alberto <albygiacomini@libero.it>
2020-06-04 01:05:42 +0200

    work on sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 {{-- <div class="bg-img"> --}}
     {{-- onmouseover="toggleSidebar()" onmouseout="toggleSidebar()" --}}
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <div id="mySidebar" class="sidebar" >
+    <div id="mySidebar" class="sidebar shadow-lg" >
         {{-- <div ><span class="tooltiptext">Tooltip text</span> --}}
         <a href="{{ route('home') }}" ><i class="material-icons">home</i><span class="icon-text"></span></a>
         <a href=""><i class="material-icons">account_balance</i><span class="icon-text"></span></a>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,14 +3,17 @@
 @section('main')
     <div id="app" class="bg-primary">
         <x-header></x-header>
-
-        <div class="container-fluid p-5">
-            <div class="container bg-white shadow-lg p-4 rounded" style="transform: translateY(-75px)">
-                <x-sidebar />
-                
-                <x-resource-container>
-                    @yield('content')
-                </x-resource-container>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-1">
+                    @include('components.sidebar')
+                </div>
+                <div class="container-fluid bg-white shadow-lg p-4 rounded col-10" style="transform: translateY(-30px)">
+                    <x-sidebar />
+                    <x-resource-container>
+                        @yield('content')
+                    </x-resource-container>
+                </div>
             </div>
         </div>
     </div>
