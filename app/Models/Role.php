3ca3dcf2f7a13d82c51385691d310d3e25c12aef
danieletulone <danieletulone.work@gmail.com>
2020-06-08 15:17:11 +0200

    Fix Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,9 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+
+	/**
+	 * Tha attributes that are mass assignable.
+	 * 
+	 * @author Daniele Tulone <danieletulone.work@gmail.com>
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'user_id',
+		'scope'
+	];
+
+	/**
+	 * List of roles defined.
+	 * 
+	 * @author Daniele Tulone <danieletulone.work@gmail.com>
+	 *
+	 * @var array
+	 */
+	protected $scopes = [
+		'admin',
+		'monument-manager',
+		'superadmin',
+		'user',
+	];
+	
+	/**
+	 * Relationship with User model
+	 * 
+	 * @author Daniele Tulone <danieletulone.work@gmail.com> 
+	 *
+	 * @return void
+	 */
 	public function users()
 	{
 		return $this->belongsTo('App\User');
 	}
-
 }
