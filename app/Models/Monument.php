9840a01f056b3e707acb4b84b8f1cf471bbf8ecd
danieletulone <danieletulone.work@gmail.com>
2020-06-16 14:48:10 +0200

    Add perPage to Monument model.

--- app/Models/Monument.php
+++ app/Models/Monument.php
@@ -6,15 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Monument extends Model
 {
+
+    /**
+     * The number of models to return for pagination.
+     *
+     * @var int
+     */
+    protected $perPage = 5;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name', 'description', 'lat', 'lon', 'user_id', 'category_id', 'visible',
     ];
 
-		protected $casts = [
-			'lat' => 'double',
-			'lon' => 'double',
-			'visible' => 'boolean'
-		];
+    protected $casts = [
+        'lat' => 'double',
+        'lon' => 'double',
+        'visible' => 'boolean'
+    ];
 
     public function user()
     {
