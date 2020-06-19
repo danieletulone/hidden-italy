2c288629aefd1e49139581570e1d120173a67245
danieletulone <danieletulone.work@gmail.com>
2020-06-19 15:31:15 +0200

    Add route for main dashboard page

--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,10 @@
 <?php
 
 Route::resource('monuments', 'MonumentController');
-
-Route::resource('monumentsCategories', 'MonumentCategoryController');
-
 Route::resource('categories', 'CategoryController');
-
 Route::resource('image', 'ImageController');
-
-Route::resource('comments', 'CommentController');
-
 Route::resource('scopes', 'ScopeController');
 Route::resource('roles', 'RoleController');
 Route::resource('users', 'UserController');
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
