275755a758dc6a63144160de5290b502e0d00ed3
danieletulone <danieletulone.work@gmail.com>
2020-06-21 20:07:54 +0200

    Add errors pages

---
+++ resources/views/errors/minimal.blade.php
@@ -0,0 +1,27 @@
+@if (Request::is("admin*") && auth()->user() != null && auth()->user()->hasScope("use-admin-dashboard"))
+    @php 
+        $layout = 'layouts.dashboard';
+        $section = 'content';
+    @endphp
+@else
+    @php 
+        $layout = 'layouts.site';
+        $section = 'main';
+    @endphp
+@endif
+
+@extends($layout)
+
+@section($section)
+    <div class="container-fluid @if($section == 'main') bg-primary @endif">
+        <div 
+            class="d-flex align-items-center justify-content-center" 
+            style="@if($section == 'main') min-height:calc(100vh - 142px) @else min-height:500px @endif"
+            >
+            <div class="text-center @if($section == 'main') text-white @endif">
+                <h1 class="display-1 font-weight-bold">@yield("code")</h1>
+                <p>@yield('message')</p>
+            </div>
+        </div>
+    </div>
+@endsection
