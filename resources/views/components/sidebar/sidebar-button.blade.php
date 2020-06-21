a073611fff7f1021d8addd76955bee238eedfd18
danieletulone <danieletulone.work@gmail.com>
2020-06-21 09:24:20 +0200

    Update sidebar-button for logout

--- resources/views/components/sidebar/sidebar-button.blade.php
+++ resources/views/components/sidebar/sidebar-button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route($route) }}" class="my-0 mx-2 my-lg-2 mx-lg-0">
+<a href="{{ route($route) }}" class="my-0 mx-2 my-lg-2 mx-lg-0" @yield('script')>
     <i class="material-icons @if(Request::is('admin/' . $name . '*')) bg-success text-white @endif">{{ $icon }}</i>
     
     @if ($name != '')
