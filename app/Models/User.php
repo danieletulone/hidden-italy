c696bad5ee9f7652f113b5b59b409044c0e221f1
danieletulone <danieletulone.work@gmail.com>
2020-06-21 14:55:24 +0200

    Fix role relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
      */
     public function role()
     {
-        return $this->belongsTo('App\Role');
+        return $this->belongsTo('App\Models\Role');
     }
 
     /**
