4791abd174826bb5d2f925b837d5cdc3555c6206
danieletulone <danieletulone.work@gmail.com>
2020-06-21 14:09:31 +0200

    Edit user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,8 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\User;
-use App\Models\Image;
 use App\Models\Role;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,18 +17,17 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->firstName,
-        'surname' => $faker->lastName,
-        'nickname' => $faker->words,
-        //'points' => 0,
-        'email' => $faker->freeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        // 'remember_token' => Str::random(10),
-        'role_id' => 2,
-        'image_id' => 1,
+$factory->define(User::class, function (Faker $faker) use ($now, $fromDate) {
 
+    $date = $faker->dateTimeBetween('-100 days', 'now');
+
+    return [
+        'firstname'  => $faker->firstName,
+        'lastname'   => $faker->lastName,
+        'email'      => $faker->freeEmail,
+        'created_at' => $date,
+        'email_verified_at' => $date,
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role_id' => Role::all()->random()->id,
     ];
 });
