e199f2e7729ce82e825339475f7330c4d7d9b30f
danieletulone <danieletulone.work@gmail.com>
2020-06-22 10:41:46 +0200

    Add new role and new scopes to user role

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -6,7 +6,71 @@ use Illuminate\Database\Seeder;
 
 class RolesTableSeeder extends Seeder
 {
-    
+
+    /**
+     * Create a role called __admin__ with all scopes without 'manage'.
+     * 
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     *
+     * @return void
+     */
+    private function defineAdmin()
+    {
+        Role::create([
+            'name' => 'admin',
+            'scopes' => Scope::select(['name'])
+                             ->where('name', 'NOT LIKE', '%manage%')
+                             ->get()
+                             ->pluck('name')
+        ]);
+    }
+
+    /**
+     * Create a role called __super-admin__ with all scopes.
+     * 
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     *
+     * @return void
+     */
+    private function defineSuperAdmin()
+    {
+        Role::create([
+            'name' => 'super-admin',
+            'scopes' => Scope::select(['name'])
+                             ->get()
+                             ->pluck('name')
+        ]);
+    }
+
+    /**
+     * Create a role that user role.
+     * 
+     * - Can read all resources.
+     * - Cannot use admin dashbaord.
+     * - Cannot delete or update resource of another user.
+     * 
+     * @author Daniele Tulone <danieletulone.work@gmail.com>
+     *
+     * @return void
+     */
+    public function defineUser()
+    {
+        $scopes = Scope::select(['name'])
+                       ->where('name', 'LIKE', '%read%')
+                       ->get()
+                       ->toArray();
+
+        $userScopes = Scope::$scopes;
+        unset($userScopes['use-admin-dashboard']);
+
+        $scopes = array_merge(array_keys($userScopes), Arr::pluck($scopes, 'name'));
+
+        Role::create([
+            'name' => 'user',
+            'scopes' => $scopes
+        ]);
+    }
+
     /**
      * Run the database seeds.
      * 
@@ -18,41 +82,8 @@ class RolesTableSeeder extends Seeder
     {
         $this->defineAdmin();
 
+        $this->defineSuperAdmin();
+
         $this->defineUser();
-    }
-
-    /**
-     * Create a role called admin with all scopes.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     *
-     * @return void
-     */
-    private function defineAdmin()
-    {
-        Role::create([
-            'name' => 'admin',
-            'scopes' => Scope::select(['name'])
-                             ->get()
-                             ->pluck('name')
-        ]);
-    }
-
-    /**
-     * Create a role that cannot create, delete or update resources.
-     * 
-     * @author Daniele Tulone <danieletulone.work@gmail.com>
-     *
-     * @return void
-     */
-    public function defineUser()
-    {
-        Role::create([
-            'name' => 'user',
-            'scopes' => Scope::select(['name'])
-                             ->where('name', 'LIKE', '%read%')
-                             ->get()
-                             ->pluck('name')
-        ]);
     }
 }
