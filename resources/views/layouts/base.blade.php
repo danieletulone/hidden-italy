5a7bf291ad86194d2bc8e0a840acbc0760438a40
Alberto <albygiacomini@libero.it>
2020-06-23 17:32:37 +0200

    Added Favicons

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('storage/assets/favicon/apple-touch-icon.png') }}" />
+        <link rel="icon" type="image/x-icon" sizes="32x32" href="{{ URL::asset('storage/assets/favicon/favicon-32x32.png') }}" />
+        <link rel="icon" type="image/x-icon" sizes="16x16" href="{{ URL::asset('storage/assets/favicon/favicon-16x16.png') }}" />
+        <link rel="icon" type="image/x-icon" href="{{ URL::asset('storage/assets/favicon/favicon-16x16.png') }}" />
+        <link rel="manifest" href="{{ URL::asset('storage/assets/favicon/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ URL::asset('storage/assets/favicon/safari-pinned-tab.svg') }}" color="#486aec">
+        <meta name="msapplication-TileColor" content="#2d89ef">
+        <meta name="theme-color" content="#ffffff">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
