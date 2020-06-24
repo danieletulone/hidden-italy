ca72f8c1f96633e6539b7cca7b5fc5742c7e7050
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-06-24 02:00:02 +0200

    Fix error in category show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -4,6 +4,10 @@
 	<x-back-button />
 	<dl class="row">
 		<dt class="col-sm-3">ID</dt>
+		<dd class="col-sm-9">{{ $category->id }}</dd>
+	</dl>
+	<dl class="row">
+		<dt class="col-sm-3">Description</dt>
 		<dd class="col-sm-9">{{ $category->description }}</dd>
 	</dl>
 </x-container>
