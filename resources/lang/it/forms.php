e6861e418c58eb57ca8609ed4ccd6fa5bafd548a
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-06-24 11:26:46 +0200

    Add lang it of forms and table of the new views

--- resources/lang/it/forms.php
+++ resources/lang/it/forms.php
@@ -14,7 +14,8 @@ return [
     // Add buttons.
     'add' => [
         'default'   => 'Aggiungi',
-        'monuments' => 'Aggiungi monumento'
+        'monuments' => 'Aggiungi monumento',
+        'roles' => 'Aggiungi'
     ],
 
     // Update buttons.
@@ -33,6 +34,12 @@ return [
         ],
         'users' => [
             'index' => 'Tutti gli utenti'
+        ],
+        'scopes' => [
+            'index' => 'Tutti i permessi'
+        ],
+        'roles' => [
+            'index' => 'Tutti i ruoli'
         ],
     ],
 ];
