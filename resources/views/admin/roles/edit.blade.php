2a88618fb23fc9dbe719490a455d8646ce98dbc2
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-06-24 03:27:15 +0200

    Add edit view.

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -9,6 +9,27 @@
 				<label for="Name">Name</label>
 				<input name="name" type="text" class="form-control" value="{{ $role->name }}">
 			</div>
+
+            <div class="form-group">
+                <label>Scopes</label>
+
+                <div>
+                    @foreach ($scopes as $scope)
+                        <div>
+                            <input 
+                                id="checkbox-{{ $scope->name }}" 
+                                type="checkbox" 
+                                name="scopes" 
+                                value="{{ $scope->name }}"      
+                            />
+                            <label for="checkbox-{{ $scope->name }}">
+                                {{ $scope->name }}
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+
 			<div class="form-group">
 				<button type="submit" class="btn btn-primary" role="button">Update Role</button>
 				<a href="{{ route('roles.index') }}" class="btn btn-secondary">Cancel</a>
