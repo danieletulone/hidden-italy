401eb316f3af2271189f7b7af228c156ac75d5c4
andreaarizzoli <andrea.arizzoli@ied.edu>
2020-06-24 12:24:08 +0200

    Add documentation.

--- app/Http/Controllers/ScopeController.php
+++ app/Http/Controllers/ScopeController.php
@@ -8,6 +8,11 @@ use App\Models\Scope;
 
 class ScopeController extends Controller
 {
+    /**
+	* Display a listing of the resource.
+	*
+	* @return \Illuminate\Http\Response
+	*/
     public function index()
     {
         return view('admin.scopes.index', [
@@ -15,16 +20,32 @@ class ScopeController extends Controller
         ]);
     }
 
+    /**
+	* Show the form for creating a new resource.
+	*
+	* @return \Illuminate\Http\Response
+	*/
     public function show(Scope $scope)
     {
         return view('admin.scopes.show')->with('scope', $scope);
     }
 
+    /**
+	* Show the form for creating a new resource.
+	*
+	* @return \Illuminate\Http\Response
+	*/
     public function create()
     {
         return view('admin.scopes.create');
     }
 
+    /**
+    * Store a newly created resource in storage.
+    * 
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(ScopeRequest $request)
     {
         Scope::create($request->all());
@@ -34,11 +55,26 @@ class ScopeController extends Controller
         return redirect()->action('ScopeController@index');
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @author Andrea Arizzoli <andrea.arizzoli@ied.edu>
+     * 
+     * @param  \App\Models\Scope  $scope
+     * @return \Illuminate\Http\Response
+     */
     public function edit(Scope $scope)
     {
         return view('admin.scopes.edit')->with('scope', $scope);
     }
 
+     /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Scope  $scope
+     * @return \Illuminate\Http\Response
+     */
     public function update(ScopeRequest $request, Scope $scope)
     {
         Scope::where('id', $scope->id)->update([
@@ -49,6 +85,12 @@ class ScopeController extends Controller
         return redirect()->action('ScopeController@index');
     }
 
+	/**
+	* Remove the specified resource from storage.
+	*
+	* @param  int  $id
+	* @return \Illuminate\Http\Response
+	*/
 	public function destroy(Scope $scope)
 	{
 		$scope->delete();
