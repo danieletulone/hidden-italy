30d19d8769199dccb0b6230704ae395fbc28d8af
danieletulone <danieletulone.work@gmail.com>
2020-06-24 15:15:17 +0200

    Table add responsiveness

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,42 +1,44 @@
-<table class="table table-striped table-hover shadow-sm">
-    <thead>
-        @foreach ($headers as $header)
-            <th>{{ __('tables.' . Str::plural($resource) . '.' . $header) }}</th>
-        @endforeach
+<div class="table-responsive">
+    <table class="table table-striped table-hover shadow-sm">
+        <thead>
+            @foreach ($headers as $header)
+                <th>{{ __('tables.' . Str::plural($resource) . '.' . $header) }}</th>
+            @endforeach
 
-        <th>{{ __('actions') }}</th>
-    </thead>
+            <th>{{ __('actions') }}</th>
+        </thead>
 
-    <tbody>
-        @foreach ($itemsData as $item)
-            <tr>
-                @foreach ($item as $key => $value)
-                    @if (!is_array($value))
-                        <td>{{ $value }}</td>
-                    @else
-                        <td></td>
-                    @endif
-                @endforeach
+        <tbody>
+            @foreach ($itemsData as $item)
+                <tr>
+                    @foreach ($item as $key => $value)
+                        @if (!is_array($value))
+                            <td>{{ $value }}</td>
+                        @else
+                            <td></td>
+                        @endif
+                    @endforeach
 
-                <td>
-                    <x-view-button
-                        :resource="$resource" 
-                        :istance="$item" 
-                    />
+                    <td class="d-flex">
+                        <x-view-button
+                            :resource="$resource" 
+                            :istance="$item" 
+                        />
 
-                    <x-edit-button
-                        :resource="$resource" 
-                        :istance="$item" 
-                    />
+                        <x-edit-button
+                            :resource="$resource" 
+                            :istance="$item" 
+                        />
 
-                    <x-delete-button 
-                        :resource="$resource" 
-                        :istance="$item"
-                    />
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+                        <x-delete-button 
+                            :resource="$resource" 
+                            :istance="$item"
+                        />
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 {{ $items->links() }}
